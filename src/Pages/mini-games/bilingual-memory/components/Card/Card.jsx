1cd193fe1683deb2677c99799c6c07bd8f3a1931
Samer taha <alrazy1@gmail.com>
2022-07-07 13:33:07 +0300

    OK OK

--- src/Pages/mini-games/bilingual-memory/components/Card/Card.jsx
+++ src/Pages/mini-games/bilingual-memory/components/Card/Card.jsx
@@ -1,26 +1,23 @@
-import "./Card.css";
-import React, { useEffect, useState } from "react";
+import './Card.css';
+//import React, { useEffect, useState } from "react";
 
 const Card = (props) => {
- 
-
-
   return (
     <div
-      className={props.isInactive ? "hidden" : ""}
-      id="CardParent"
-      onClick={() => {        
-        props.onClick(props.index); 
-      }}>
+      className={props.isInactive ? 'hidden' : ''}
+      id='CardParent'
+      onClick={() => {
+        props.onClick(props.index);
+      }}
+    >
       {props.isFlipped ? (
-        <div className="back ">
+        <div className='back '>
           <p className={props.isArabic ? `arabicCard` : null}>{props.text}</p>
         </div>
       ) : (
-        <div className="front"></div>
+        <div className='front'></div>
       )}
     </div>
-
   );
 };
 
--- src/Pages/mini-games/bilingual-memory/components/GameBoardMemory/GameBoardMemory.jsx
+++ src/Pages/mini-games/bilingual-memory/components/GameBoardMemory/GameBoardMemory.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState, useRef } from "react";
-import Card from "../Card/Card";
-import "./GameboardMemory.css";
-import { shuffleRandomly } from "../../../../../utils/utils";
-import TurnPanel from "../TurnPanel/TurnPanel";
-import gotchya from "../../../../../api/gotchyaApi";
+import React, { useEffect, useState, useRef } from 'react';
+import Card from '../Card/Card';
+import './GameboardMemory.css';
+import { shuffleRandomly } from '../../../../../utils/utils';
+import TurnPanel from '../TurnPanel/TurnPanel';
+import gotchya from '../../../../../api/gotchyaApi';
 
 const GameBoardMemory = (props) => {
   const [cards, setCards] = useState([]);
   const [openCards, setOpenCards] = useState([]);
   const [clearedCards, setClearedCards] = useState({});
-  const [moves, setMoves] = useState(0);
+  //const [moves, setMoves] = useState(0);
   const [pairsLeft, setPairsLeft] = useState(6);
   const [turn, setTurn] = useState(true);
-  const [preventSecondClick, setPreventSecondClick] = useState(true);
+  //const [preventSecondClick, setPreventSecondClick] = useState(true);
   const [disableClicking, setDisableClicking] = useState(false);
   const timeout = useRef(null);
 
@@ -20,15 +20,15 @@ const GameBoardMemory = (props) => {
     if (pairsLeft === 0) {
       props.isDone(true);
     }
-  }, [pairsLeft]);
+  }, [pairsLeft, props]);
 
   useEffect(() => {
     getWords();
     randomizeAndFill();
-  }, []);
+  }, [randomizeAndFill]);
 
   const getWords = async () => {
-    let { data } = await gotchya.get("/wordBank");
+    let { data } = await gotchya.get('/wordBank');
     return data;
   };
 
@@ -56,14 +56,13 @@ const GameBoardMemory = (props) => {
 
   // Check if both the words have same id. If so, mark them inactive
   const evaluate = () => {
-    
     const [first, second] = openCards;
     if (cards[first].id === cards[second].id) {
       // if (pairsLeft === 0) {
       //   alert("Good Job!"); // winning scenario here
       //   return;
       // }
-      
+
       setPairsLeft((prev) => prev - 1);
       setClearedCards((prev) => ({ ...prev, [cards[first].id]: true }));
       setOpenCards([]);
@@ -73,15 +72,14 @@ const GameBoardMemory = (props) => {
       setOpenCards([]);
     }, 500);
     setTimeout(() => {
-      
       setDisableClicking(false);
     }, 500);
-console.log(disableClicking);
+    console.log(disableClicking);
   };
 
   const handleCardClick = (index) => {
     // Have a maximum of 2 items in array at once.
-    setPreventSecondClick(true)
+    setPreventSecondClick(true);
     if (openCards.length === 1) {
       setOpenCards((prev) => [...prev, index]);
       // increase the moves once we opened a pair
@@ -99,7 +97,7 @@ console.log(disableClicking);
       setDisableClicking(true);
       setTimeout(evaluate, 500);
     }
-  }, [openCards]);
+  }, [openCards, evaluate]);
 
   const checkIsFlipped = (index) => {
     return openCards.includes(index);
@@ -111,20 +109,19 @@ console.log(disableClicking);
     return disableClicking;
   };
   const dummyFunc = () => {
-    
     return;
   };
 
   const drawBoard = (words) => {
     return words.map((word, i) => {
       return (
-        <div  key={i}>
+        <div key={i}>
           <Card
             text={word.word}
             id={word.id}
             index={i}
-            isArabic={word.isArabic || ""}
-            onClick={(!disableClicking && handleCardClick ) || dummyFunc }
+            isArabic={word.isArabic || ''}
+            onClick={(!disableClicking && handleCardClick) || dummyFunc}
             isInactive={checkIsInactive(word)}
             isFlipped={checkIsFlipped(i)}
             isDisabled={checkIsDisabled()}
@@ -137,7 +134,7 @@ console.log(disableClicking);
   return (
     <>
       <TurnPanel isPlaying={turn} />
-      <div className="GameBoardMemory">{drawBoard(cards)}</div>
+      <div className='GameBoardMemory'>{drawBoard(cards)}</div>
       <TurnPanel buttom={true} isPlaying={!turn} />
     </>
   );
