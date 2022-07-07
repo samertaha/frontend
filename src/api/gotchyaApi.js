90eeb035aa4bd52db96943dc62bc173c147dcef9
Samer taha <alrazy1@gmail.com>
2022-07-07 15:44:41 +0300

    GGGGGGGGG

--- src/api/gotchyaApi.js
+++ src/api/gotchyaApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const gotchya = axios.create({
-  baseURL: 'http://localhost:8000/api',
-  // baseURL: 'ec2-3-141-1-43.us-east-2.compute.amazonaws.com/api',
+  // baseURL: 'http://localhost:8000/api',
+  baseURL: 'http://ec2-3-141-1-43.us-east-2.compute.amazonaws.com/api',
 });
 
 export default gotchya;
