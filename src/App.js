267e6860fda640989668f731b49cfdd2e23df950
Samer taha <alrazy1@gmail.com>
2022-07-07 11:17:32 +0300

    removed copy from yml

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 function App() {
   return (
     <div>
-      <h1>Continious deployment 9:00 AM</h1>
+      <h1>
+        Continious deployment 9:00 AM copy build to /var/www/html removed from
+        yml
+      </h1>
     </div>
   );
 }
