7a396419db0b17498dae5ec8916575cda2305209
Samer taha <alrazy1@gmail.com>
2022-11-09 16:47:13 +0200

    changed baseURL to aws api

--- src/api/gotchyaApi.js
+++ src/api/gotchyaApi.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const gotchya = axios.create({
-  //baseURL: 'http://localhost:8000/api',
-  baseURL: 'http://ec2-3-134-252-141.us-east-2.compute.amazonaws.com/api',
+  //baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://ec2-3-28-119-168.me-central-1.compute.amazonaws.com/api',
 });
 
 export default gotchya;
